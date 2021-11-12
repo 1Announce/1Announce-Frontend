62989ddc6a8889852cb66f393d52d65c97cf7cde
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-11-11 19:29:07 -0600

    updated firebase.js to remove api keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBaRLGZvIJY7YZuCHugptmfk0_2FosBU1M",
-  authDomain: "announce-d9b77.firebaseapp.com",
-  projectId: "announce-d9b77",
-  storageBucket: "announce-d9b77.appspot.com",
-  messagingSenderId: "252168177348",
-  appId: "1:252168177348:web:83d700cc72aabefb24eb91",
-  measurementId: "G-H0Q0Y49KQB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_AP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
 })
 
 export const auth = app.auth()
