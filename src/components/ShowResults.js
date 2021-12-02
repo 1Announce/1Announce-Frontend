139127377bcccaaa419ef412c41f6a29ad41f4eb
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-12-02 16:14:41 -0600

    Fixed tables to show announcement schedule time, and redirect

--- src/components/ShowResults.js
+++ src/components/ShowResults.js
@@ -2,5 +2,4 @@ import ApiManager from "../api/api-manager";
 
 export default (async function showResults(values, id) {
   ApiManager.createAnnouncement(id, values);
-  window.alert(`You submitted:\n\n${JSON.stringify(values, 1, 2)}`);
 });
